55f68c38977a6c634e2220ea79736bba67d05464
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-11-23 14:23:46 -0300

    Projeto criado

---
+++ src/main/java/com/devsuperior/wsmongodb/WsMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.wsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/wsmongodb/WsMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.wsmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
