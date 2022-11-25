f67931cf86f4d4b88695a2f908e2377df3bfc2df
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-11-25 11:13:27 -0300

    Inserção de usuário com POST

--- src/main/java/com/devsuperior/wsmongodb/services/UserService.java
+++ src/main/java/com/devsuperior/wsmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.wsmongodb.domain.User;
+import com.devsuperior.wsmongodb.dto.UserDTO;
 import com.devsuperior.wsmongodb.repository.UserRepository;
 import com.devsuperior.wsmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
