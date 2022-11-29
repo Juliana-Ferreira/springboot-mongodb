ba433f58ed5f1b935e903d188613fc12f50bc192
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-11-29 10:01:44 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/devsuperior/wsmongodb/config/Instantiation.java
+++ src/main/java/com/devsuperior/wsmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.devsuperior.wsmongodb.domain.Post;
 import com.devsuperior.wsmongodb.domain.User;
 import com.devsuperior.wsmongodb.dto.AuthorDTO;
+import com.devsuperior.wsmongodb.dto.CommentDTO;
 import com.devsuperior.wsmongodb.repository.PostRepository;
 import com.devsuperior.wsmongodb.repository.UserRepository;
 
@@ -40,8 +41,16 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
